b5d85247970841abae23839ed52993ce8db6e8b5
litexcoin <41809806+litexcoin@users.noreply.github.com>
2018-07-28 02:20:38 -0400

    update installer

--- ltx_installer.sh
+++ ltx_installer.sh
@@ -6,7 +6,7 @@ CONFIGFOLDER='/root/.ltx'
 COIN_DAEMON='ltxd'
 COIN_CLI='ltx-cli'
 COIN_PATH='/usr/local/bin/'
-COIN_TGZ='https://github.com/litexcoin/ltx/archive/v2.2.0.3.tar.gz'
+COIN_TGZ='https://github.com/litexcoin/ltx/releases/download/v2.2.0.3/ltx-2.2.0-x86_64-linux-wallet.tar.gz'
 COIN_ZIP=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
 COIN_NAME='ltx'
 COIN_PORT=17991
@@ -195,14 +195,14 @@ fi
 function prepare_system() {
 echo -e "Prepare the system to install ${GREEN}$COIN_NAME${NC} master node."
 apt-get update >/dev/null 2>&1
-# DEBIAN_FRONTEND=noninteractive apt-get update > /dev/null 2>&1
-# DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y -qq upgrade >/dev/null 2>&1
-# apt install -y software-properties-common >/dev/null 2>&1
-# echo -e "${GREEN}Adding bitcoin PPA repository"
-# apt-add-repository -y ppa:bitcoin/bitcoin >/dev/null 2>&1
-# echo -e "Installing required packages, it may take some time to finish.${NC}"
-# apt-get update >/dev/null 2>&1
-apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" 
+DEBIAN_FRONTEND=noninteractive apt-get update > /dev/null 2>&1
+DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y -qq upgrade >/dev/null 2>&1
+apt install -y software-properties-common >/dev/null 2>&1
+echo -e "${GREEN}Adding bitcoin PPA repository"
+apt-add-repository -y ppa:bitcoin/bitcoin >/dev/null 2>&1
+echo -e "Installing required packages, it may take some time to finish.${NC}"
+apt-get update >/dev/null 2>&1
+apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" make software-properties-common \
 build-essential libtool autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev libboost-program-options-dev \
 libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake git wget curl libdb4.8-dev bsdmainutils libdb4.8++-dev \
 libminiupnpc-dev libgmp3-dev ufw pkg-config libevent-dev  libdb5.3++ libzmq5 >/dev/null 2>&1
@@ -211,7 +211,7 @@ if [ "$?" -gt "0" ];
     echo -e "${RED}Not all required packages were installed properly. Try to install them manually by running the following commands:${NC}\n"
     echo "apt-get update"
     echo "apt -y install software-properties-common"
-    # echo "apt-add-repository -y ppa:bitcoin/bitcoin"
+    echo "apt-add-repository -y ppa:bitcoin/bitcoin"
     echo "apt-get update"
     echo "apt install -y make build-essential libtool software-properties-common autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev \
 libboost-program-options-dev libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake git curl libdb4.8-dev \
